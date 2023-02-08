90a15616d006dc5494586dc3f303a9e90b622649
Antony <officialantony23@gmail.com>
2023-02-08 22:09:46 +0300

    committed

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] arg){
+        double Inches=convertToCentimeters(17);
+        System.out.println(Inches +" Cm");
+       double JustInches= convertToCentimeters(5,8);
+       System.out.println(JustInches +" Inches");
+    }
+    public static double convertToCentimeters(int HeightInch){
+        System.out.println(HeightInch +" Inches,Converted to Centimeters = " );
+        return HeightInch *2.54;
+    }
+    public static double convertToCentimeters(int HeightFeet,int HeightInch){
+        System.out.println(HeightFeet +" Feet and "+ HeightInch +" Inches, Converted to just inches =");
+        return (HeightFeet * 12) +HeightInch;
     }
 }
